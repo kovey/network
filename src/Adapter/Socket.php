bbaef22995c9518401056ebc12e298051e358aed
kovey <kovey@vip.qq.com>
2022-06-10 18:13:05 +0800

    bugs fixed

--- src/Adapter/Socket.php
+++ src/Adapter/Socket.php
@@ -73,7 +73,7 @@ class Socket extends Base
             return;
         }
 
-        $this->close->close(new Event\Close($this, $event->fd, $event->reactorId));
+        $this->close->close(new Event\Close($this, $event->fd, $event->reactor_id));
     }
 
     public function send(PacketInterface $packet, int $fd) : bool
